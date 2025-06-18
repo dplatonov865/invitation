827e0f1d2f5a3801aeaab69c8c576a842b2ec392
Anastasia <sholloway591@gmail.com>
2025-06-18 19:36:02 +0300

    Шрифты

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,10 @@ function Header() {
   return (
     <header className="header">
       <h1>Мы рады пригласить Вас на нашу свадьбу! </h1>
-      <img src={datePhoto} alt={`Фото c датой`} className="header-image"/>
-      <h1> </h1>
+      <div className="date">
+          <h1>16 августа 2025</h1>
+{/*       <img src={datePhoto} alt={`Фото c датой`} className="header-image"/> */}
+      </div>
       <h1>Дмитрий и Анастасия</h1>
       <img src={photo} alt={`Фото c голубями`} className="header-photo"/>
     </header>
